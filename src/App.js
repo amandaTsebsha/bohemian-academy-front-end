d5207103f26c1ba59df7e4611e1b3e1c005ead4a
amynollz153@gmail.com <amynollz153@gmail.com>
2024-11-21 16:43:16 +0200

    Add Frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data')
+    .then(response => { setData(response.data);
+      
+    }).catch(error => console.log(error));
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>
+          {data ? data.message : 'Loading..'}
+        </h1>
+        <h2>Our Offerings: </h2>
+        <ul>
+          {data &&data.offerings.map((offering, index) => (
+            <li key={index}>{offering}</li>
+          ))}
+        </ul>
+        <button>{data && data.cta}</button>
       </header>
     </div>
   );
